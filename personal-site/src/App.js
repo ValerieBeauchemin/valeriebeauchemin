23faa2e85f280669145d9a0bdec1c5ef8c828065
ValerieBeauchemin <valerie.d.beauchemin@gmail.com>
2024-01-24 13:05:37 -0500

    add animations and fonts

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useEffect } from 'react';
+import Granim from "granim";
+import logo from './logo.webp';
+import background from './background.jpg';
+import AOS from 'aos';
 
 function App() {
+  useEffect(() => {
+    new Granim ({
+      element: "#canvas",
+      direction: "radial",
+      image: {
+        source: background,
+        blendingMode: "multiply",
+        stretchMode: ["stretch-if-bigger", "stretch-if-bigger"],
+      },
+      states: {
+        "default-state": {
+          gradients: [
+            ["#7C3BBE", "#3BBE7C"],
+            ["#3BBE7C", "#BE7C3B"],
+            ["#BE7C3B", "#7C3BBE"],
+          ],
+          transitionSpeed: 2000
+        }
+      }
+    });
+  }, []);
+  
+  AOS.init();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas id="canvas" className="canvas"></canvas>
+      <img className="logo-mask" src={logo} alt="logo" />
+      <div className="App__text-content">
+        <header className="App__title">ChronoSphere</header>
+        <p className="App__subtitle">Time Tours</p>
+      </div>
     </div>
   );
 }
